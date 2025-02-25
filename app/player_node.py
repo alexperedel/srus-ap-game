3b3ae3c37e92bc961d3309bb9adc7258ab38f8a1
ALEKSANDR PEREDELSKII <PEREDA@tdm.local>
2025-02-25 14:29:43 +0800

    Add player_node.py and class PlayerNode with it's properties

---
+++ app/player_node.py
@@ -0,0 +1,32 @@
+class PlayerNode:
+    def __init__(self, player):
+        self._player = player
+        self._next = None
+        self._previous = None
+
+    @property
+    def player(self):
+        return self._player
+
+    @property
+    def next(self):
+        return self._next
+
+    @next.setter
+    def next(self, value):
+        self._next = value
+
+    @property
+    def previous(self):
+        return self._previous
+
+    @previous.setter
+    def previous(self, value):
+        self._previous = value
+
+    @property
+    def key(self):
+        return self._player.uid
+
+    def __str__(self):
+        return f"Player id: {self.key}, next {self.next}, previous {self.previous}"
