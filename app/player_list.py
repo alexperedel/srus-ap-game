e24c5372c05694a86a18e9cc9b9a4587304d43be
ALEKSANDR PEREDELSKII <PEREDA@tdm.local>
2025-02-25 15:07:30 +0800

    Start working on append method

--- app/player_list.py
+++ app/player_list.py
@@ -1,3 +1,30 @@
+from player_node import PlayerNode
+
 class PlayerList:
     def __init__(self):
         self._head = None
+        self._tail = None
+
+    @property
+    def head(self):
+        return self._head
+
+    @property
+    def tail(self):
+        return self._tail
+
+    def is_empty(self):
+        if self._head is None:
+            return True
+
+        return False
+
+    def append(self, player):
+        if self.is_empty():
+            self._head, self._tail = PlayerNode(player)
+
+        else:
+            player_node = PlayerNode(player)
+            
+
+
