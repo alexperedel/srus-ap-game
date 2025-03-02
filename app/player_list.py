e1141817027d5bd3b649a4dce5f52038466eeefc
Alex <sashavalera35@gmail.com>
2025-03-02 21:43:07 +0800

    Add 'append_right' method and add unit tests for it

--- app/player_list.py
+++ app/player_list.py
@@ -20,16 +20,27 @@ class PlayerList:
 
         return False
 
-    def append(self, player):
+    def _add_first_node(self, node):
+        self._head = node
+        self._tail = node
+
+    def append_left(self, player):
         if self.is_empty():
-            node = PlayerNode(player)
-            self._head = node
-            self._tail = node
+            self._add_first_node(PlayerNode(player))
         else:
             player_node = PlayerNode(player)
             player_node.next = self._head
             self.head.previous = player_node
             self._head = player_node
+
+    def append_right(self, player):
+        if self.is_empty():
+            self._add_first_node(PlayerNode(player))
+        else:
+            player_node = PlayerNode(player)
+            player_node.previous = self._tail
+            self.tail.next = player_node
+            self._tail = player_node
             
 
 
--- test/player_list_test.py
+++ test/player_list_test.py
@@ -5,11 +5,11 @@ from app.player import Player
 
 
 class TestPlayerList(unittest.TestCase):
-    def test_successfully_append_player_empty_list(self):
+    def test_successfully_append_left_player_empty_list(self):
         player = Player("20000", "John")
         player_list = PlayerList()
 
-        player_list.append(player)
+        player_list.append_left(player)
 
         head = player_list.head
         tail = player_list.tail
@@ -17,24 +17,50 @@ class TestPlayerList(unittest.TestCase):
         self.assertEqual(head.key, player.uid)
         self.assertEqual(head, tail)
 
-    def test_successfully_append_player_not_empty_list(self):
+    def test_successfully_append_left_player_not_empty_list(self):
         player = Player("20000", "John")
         player_1 = Player("50000", "Tom")
 
         player_list = PlayerList()
 
-        player_list.append(player)
-        player_list.append(player_1)
+        player_list.append_left(player)
+        player_list.append_left(player_1)
 
         head = player_list.head
         tail = player_list.tail
 
         self.assertEqual(head.key, player_1.uid)
+        self.assertEqual(tail.key, player.uid)
+        self.assertEqual(head.next.key, player.uid)
+        self.assertEqual(tail.previous.key, player_1.uid)
+
+    def test_successfully_append_right_player_empty_list(self):
+        player = Player("20000", "John")
+        player_list = PlayerList()
+
+        player_list.append_right(player)
+
+        head = player_list.head
+        tail = player_list.tail
 
         self.assertEqual(tail.key, player.uid)
+        self.assertEqual(tail, head)
 
-        self.assertEqual(head.next.key, player.uid)
+    def test_successfully_append_right_player_not_empty_list(self):
+        player = Player("20000", "John")
+        player_1 = Player("50000", "Tom")
 
-        self.assertEqual(tail.previous.key, player_1.uid)
+        player_list = PlayerList()
+
+        player_list.append_right(player)
+        player_list.append_right(player_1)
+
+        head = player_list.head
+        tail = player_list.tail
+
+        self.assertEqual(head.key, player.uid)
+        self.assertEqual(tail.key, player_1.uid)
+        self.assertEqual(head.next.key, player_1.uid)
+        self.assertEqual(tail.previous.key, player.uid)
 
 
